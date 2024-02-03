ed200bfd3f5fd66bb0b0a545b600c63bd5ab8dc6
ProgrammerNelitha <1234legobrick@gmail.com>
2024-02-03 14:30:35 -0500

    rot and n added

--- Nelitha-test.py
+++ Nelitha-test.py
@@ -1,61 +1,168 @@
+import numpy as np
+import math
+from scipy.io import wavfile
+#GOALS:
+#GENERATE A LIST OF FREQUENCY, VOLUME FOR EVERY FRAME
+def roots(lst):
+    rootNo = 0
+    for i in range(0,len(lst)):
+        if abs(lst[i]) < 75 : #low amplitude..?
+            rootNo=rootNo+1
+    return rootNo
+
+def turningPoints(lst):
+    dx = np.diff(lst)
+    return dx[1:] * dx[:-1] < 0
+def turningPointsSum(lst):
+    dx = np.diff(lst)
+    return np.sum(dx[1:] * dx[:-1] < 0)
+# samplerate, data = wavfile.read("C:\\Users\\jackj\\Downloads\\Cute Bell Sound Effect.wav")
+# print(samplerate)
+# print(data.shape[0])
+# n = 0
+#samplerate, data = wavfile.read("C:\\Users\\jackj\\OneDrive\\Documents\\sound files\\C.wav")
+samplerate, data = wavfile.read("C:\\Users\\1234l\\Downloads\\cfbc.wav")
+#samplerate, data = wavfile.read("D:\\tartnhack\\testingnowork.wav")
+#samplerate, data = wavfile.read("D:\\tartnhack\\c scale.wav")
+
+#samplerate, data = wavfile.read("D:\\tartnhack\\track.wav")
+#samplerate, data = wavfile.read("D:\\tartnhack\\Major Scale.wav")
+print(f"number of channels = {data.shape[1]}")
+length = data.shape[0] / samplerate
+print(f"length = {length}s")
+print(data.shape[0])
+fps = 30
+timePerFrame = 1/fps
+samplesLeft = data.shape[0]
+samplesPerFrame = samplerate//fps
+framesTotal = math.floor(fps*length) #imprecise?
+currentFrame = 0
+# cLight = 299,792,458
+#while we are still in a whole frame, do this
+volumeList = []
+frequencyList = []
+nList = []
+qualityList = []
+maxPtsList = []
+turningPointReady = True
+print(samplesPerFrame)
+while currentFrame != framesTotal : 
+    currentVolume = 0
+    sampleList1d = []
+    # sampleList1dR = []
+    for i in range(samplesPerFrame*currentFrame,samplesPerFrame*(currentFrame+1)):
+        sampleList1d.append(data[i][0])
+    for i in range(0,samplesPerFrame):
+        currentVolume = currentVolume + np.abs(sampleList1d[i])  #mono
+    
+    #print(sampleList1d)
+    # turningPtsPerFrameL = turningPoints(sampleList1dL)
+    # turningPtsPerFrameR = turningPoints(sampleList1dR)
+    # turningPtsPerFrame = (turningPtsPerFrameL+turningPtsPerFrameR)//2
+    #print("This?")
+    currentQuality = turningPointsSum(sampleList1d)
+    # currentQuality = (qualityR + qualityL)//2
+    turningPtsPerFrameList = turningPoints(sampleList1d)
+    # turningPtsPerFrameR = turningPoints(sampleList1dR)
+    turningPtsPerFrame = 0
+    maxPts = 0
+
+    
+    for i in range(0,samplesPerFrame-2): #this is okay because there are a lot of samples
+        if turningPtsPerFrameList[i] and sampleList1d[i] > 3000 and turningPointReady :
+            turningPtsPerFrame = turningPtsPerFrame + 1
+            turningPointReady = not turningPointReady
+            if sampleList1d[i] > 10000: #what is a "high" volume?
+                maxPts = maxPts + 1
+        if turningPtsPerFrameList[i] and sampleList1d[i] < 3000 and not turningPointReady :
+            turningPtsPerFrame = turningPtsPerFrame + 1
+            turningPointReady = not turningPointReady
+    turningPtsPerFrame = turningPtsPerFrame//2
+    nList.append(turningPtsPerFrame)
+    if (turningPtsPerFrame >= 1) :
+        period = timePerFrame/(turningPtsPerFrame)
+        frequencyList.append(1//period)
+    else :
+        frequencyList.append(0)
+    if (maxPts > 5) :
+        maxPtsList.append(True)
+    else :
+        maxPtsList.append(False)
+    volumeList.append(currentVolume)
+    qualityList.append(currentQuality)
+    currentFrame=currentFrame+1
+frameList = []
+for i in range(0,framesTotal) :
+    frameList.append(i)
+    if frequencyList[i] < 50:
+        volumeList[i] = 0
+    if volumeList[i] < 50000:
+        qualityList[i] = 0
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# plt.plot(frameList, frequencyList, label="Left channel")
+# plt.legend()
+# plt.xlabel("frame")
+# plt.ylabel("frq")
+# plt.show()
+
 import tkinter as tk
+import time
+import math
+
+
+'''
+volume=how fast r increases
+frequency= color ~200-1000
+quality=saturation ~400-1000
+nList= n. Not one to one. if nList between 20-30, n=6 for example. 3-30
+maxPoints = fireworks??
+'''
+
+# delay between successive frames in seconds
+animation_refresh_seconds = 0.01
 
 m = tk.Tk()
 m.geometry("700x700")
-#m.background("gray")
-m.title("Hey")
-
-
-C = tk.Canvas(m,width=700,height=700)
-#oval = C.create_oval(80, 30, 140, 150, fill="blue")
-#C.create_arc(150,200,550,500,extent =900,outline ="black",fill ="blue", width =2, style=tk.CHORD)
-C.create_line(350,0,350,700, fill="black", width=1)
-C.create_line(0,350,700,350, fill="black", width=1)
-C.create_line(0,0,700,700, fill="black", width=1)
-C.create_line(700,0,0,700, fill="black", width=1)
-
-def callback(e):
-   x= e.x
-   y= e.y
-   print("Pointer is currently at %d, %d" %(x,y))
-m.bind('<Motion>',callback)
-
-
-def create_circle(x,y,r,color):
-   x1=x-r
-   y1=y-r
-   x2=x+r
-   y2=y+r
-   C.create_oval(x1,y1,x2,y2,fill=color, outline=color)
-
-#C.create_arc(200,200,350,350,extent=330,outline ="blue",fill ="blue", width =2, style=tk.PIESLICE)
-#C.create_oval(100,10,200,110)
-def drawOval(x1,y1,x2,y2, c):
-   C.create_oval(x1,y1,x2,y2,fill =c, outline=c)
-   C.create_oval(y1,x1,y2,x2,fill =c, outline=c)
-   C.create_oval(y1,700-x1,y2,700-x2,fill =c, outline=c)
-   C.create_oval(x1,700-y1,x2,700-y2,fill =c, outline=c)
-   C.create_oval(700-x1,700-y1,700-x2,700-y2,fill =c, outline=c)
-   C.create_oval(700-y1,700-x1,700-y2,700-x2,fill =c, outline=c)
-   C.create_oval(700-x1,y1,700-x2,y2,fill =c, outline=c)
-   C.create_oval(700-y1,x1,700-y2,x2,fill =c, outline=c)
-
-def drawCircle(x,y,r,c):
-   create_circle(x,y,r,c)
-   create_circle(y,x,r,c)
-   create_circle(y,700-x,r,c)
-   create_circle(x,700-y,r,c)
-   create_circle(700-x,700-y,r,c)
-   create_circle(700-y,700-x,r,c)
-   create_circle(700-x,y,r,c)
-   create_circle(700-y,x,r,c) 
-
-drawOval(100,10,200,110,"blue")
-drawOval(250,230,340,290,"red")   
-drawOval(70,260,200,330,"green")
-drawOval(0,300,40,320,"yellow")
-drawOval(250,230,340,290,"orange")
-
+m.title("Sound to Art Converter")
+C = tk.Canvas(m,width=700,height=700,background="black")
 C.pack()
+
+#starting number of points
+n=3
+dn=0
+#starting radius in pixels
+r=50
+dr=0
+#starting rotation factor
+rot=0
+drot=0.05
+
+num_frames = len(volumeList)
+max_vol = max(volumeList)
+min_vol = min(volumeList)
+#print(f"max vol is: {max_vol}")
+k=0
+colorList=["red","orange","yellow","green","blue","white"]
+while k<num_frames:
+    r=(volumeList[k]) * (300/max_vol)
+    n=nList[k]
+    if n<3: n=3
+    #color = freq_to_color(frequencyList[k])
+    rot+=0.05
+    
+    coords=[]
+    for i in range(n):
+        coords.append(350+r*math.cos((rot+2*math.pi*i)/n))
+        coords.append(350+r*math.sin((rot+2*math.pi*i)/n))
+
+    C.create_polygon(coords, fill="", width=3, outline=colorList[k%6], tag=f"{k}")
+    if k>60: C.delete(f"{k-60}")
+    m.update()
+    time.sleep(animation_refresh_seconds)
+    k+=1
+
 
 m.mainloop()

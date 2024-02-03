eeb346638ee5bb5dbdee213f30cc570347f7b81d
ProgrammerNelitha <1234legobrick@gmail.com>
2024-02-03 00:42:06 -0500

    Update spiral-test.py

--- spiral-test.py
+++ spiral-test.py
@@ -6,7 +6,17 @@ m.geometry("700x700")
 m.title("Hey")
 
 
+
 C = tk.Canvas(m,width=700,height=700)
+
+def callback(e):
+   x= e.x
+   y= e.y
+   print("Pointer is currently at %d, %d" %(x,y))
+m.bind('<Motion>',callback)
+
+
+m.mainloop()
 
 #define the endpoints of a ngon
 n=4

521e1d2294039ef168036f5b7556ff811eae48d2
ProgrammerNelitha <1234legobrick@gmail.com>
2024-02-03 14:11:37 -0500

    added rot and nList

--- image-gen.py
+++ image-gen.py
@@ -7,7 +7,7 @@ import math
 volume=how fast r increases
 frequency= color ~200-1000
 quality=saturation ~400-1000
-nList= n. Not one to one. if nList between 20-30, n=6 for example
+nList= n. Not one to one. if nList between 20-30, n=6 for example. 3-30
 maxPoints = fireworks??
 '''
 
@@ -34,23 +34,21 @@ num_frames = len(volumeList)
 max_vol = max(volumeList)
 min_vol = min(volumeList)
 
-def create_shape(Cv,coordList, color):
-    Cv.create_polygon(coordList, fill="", width=3, outline=color)
-
 k=0
 colorList=["red","orange","yellow","green","blue","white"]
 while k<num_frames:
-    coords=[]
-
     r=volumeList[k] * 300/max_vol
+    n=nList[k]
     #color = freq_to_color(frequencyList[k])
-
+    rot+=0.05
+    
+    coords=[]
     for i in range(n):
         coords.append(350+r*math.cos((rot+2*math.pi*i)/n))
         coords.append(350+r*math.sin((rot+2*math.pi*i)/n))
 
-
-    create_shape(C,coords,colorList[k%6])
+    C.create_polygon(coords, fill="", width=3, outline=colorList[k%6], tag=f"{k}")
+    if k>60: C.delete(f"{k-60}")
     m.update()
     time.sleep(animation_refresh_seconds)
     k+=1

b884377042b21e595d3cf86fe983a56e0c7cdf34
ProgrammerNelitha <1234legobrick@gmail.com>
2024-02-03 11:57:24 -0500

    a configuration that I like

--- spiral-test2.py
+++ spiral-test2.py
@@ -23,50 +23,32 @@ C = tk.Canvas(m,width=700,height=700)
 C.pack()
 
 #number of points
-n=4
+n=3
 #radius in pixels
-r=100
-#rotation factor
+r=50
+#rotation factors
 rot=0
 
-def create_shape(n,C,x_coords,y_coords, color):
-    if n==3:
-        C.create_polygon(x_coords[0],y_coords[0],x_coords[1],y_coords[1],x_coords[2],y_coords[2],x_coords[3],y_coords[3], fill="", outline=color)
-    if n==4:
-        C.create_polygon(x_coords[0],y_coords[0],x_coords[1],y_coords[1],x_coords[2],y_coords[2],x_coords[3],y_coords[3], fill="", outline=color)
-
-
-x_coords=[]
-y_coords=[]
-for i in range(4):
-    x_coords.append(350+r*math.cos((2*math.pi*i)/n))
-    y_coords.append(350+r*math.sin((2*math.pi*i)/n))
-print(x_coords,y_coords)
-
+def create_shape(n,Cv,coordList, color):
+    Cv.create_polygon(coordList, fill="", width=3, outline=color)
 
 k=0
-while k<150:
-    x_c=[]
-    y_c=[]
-    r+=10
-    rot+=1
+while k<70:
+    coords=[]
+    r+=5
+    rot+=0.05
+    if k%10==0 : n+=1
+
     for i in range(n):
-        x_c.append(350+r*math.cos((rot+2*math.pi*i)/n))
-        y_c.append(350+r*math.sin((rot+2*math.pi*i)/n))
+        coords.append(350+r*math.cos((rot+2*math.pi*i)/n))
+        coords.append(350+r*math.sin((rot+2*math.pi*i)/n))
 
-    C.create_polygon(x_c[0],y_c[0],x_c[1],y_c[1],x_c[2],y_c[2],x_c[3],y_c[3], fill="", outline='black')
+
+    create_shape(n,C,coords,"black")
     m.update()
     time.sleep(animation_refresh_seconds)
     k+=1
+    print(n)
 
-
-'''
-k=0
-while k<150:
-    C.create_line(350+10*k,0,350+10*k,700, fill="black", width=1)
-    m.update()
-    time.sleep(animation_refresh_seconds)
-    k+=1
-'''   
 
 m.mainloop()

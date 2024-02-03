23a4f5f7c9cb4935967585b331b9d6ab803a5331
henrysiegel17 <67601076+henrysiegel17@users.noreply.github.com>
2024-02-03 14:12:43 -0500

    Frequency -> Color function is under graphics.py

---
+++ graphics.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+ROOT = tk.Tk()
+ROOT.geometry("700x700")
+C = tk.Canvas(ROOT,width=700,height=700,background='#999900')
+C.pack()
+
+ROOT.mainloop()
---
+++ sound.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from scipy.fft import fft, rfft, fftfreq
+from scipy.io import wavfile # get the api
+
+SAMPLE_RATE = 44100  # Hertz
+DURATION = 5  # Seconds
+_, data = wavfile.read("/Users/henrysiegel/Downloads/C.wav")
+
+
+RGB = (255,255,255)
+
+def frequencyToRGB(frequency):
+    # min: 0
+    # max: 2000
+
+    if (frequency > 2000):
+        return (0,0,255)
+    elif (frequency < 1000):
+        return (255-(frequency/1000 * 255), (frequency/1000 * 255), 0)
+    else: 
+        return (0, 255-((frequency-1000)/1000 * 255),  ((frequency-1000)/1000 * 255))
